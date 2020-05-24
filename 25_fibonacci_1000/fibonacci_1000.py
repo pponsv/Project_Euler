909ec61ea037395489596225178e4be352a3d4b3
pponsv <ponsvpedro@gmail.com>
2020-05-24 17:29:55 +0200

    Añadido algoritmo de búsqueda de ceros (biseccion) al problema 25

--- 25_fibonacci_1000/fibonacci_1000.py
+++ 25_fibonacci_1000/fibonacci_1000.py
@@ -8,9 +8,9 @@ What is the index of the first term in the Fibonacci sequence to contain
 
 
 def fi(n):
-    init = [1,1]
+    init = [1, 1]
     N = 0
-    for i in range(2,n):
+    for i in range(2, n):
         N = sum(init)
         init[0] = init[1]
         init[1] = N
@@ -20,14 +20,24 @@ def fi(n):
 def lstr(n):
     return len(str(n))
 
+
 N = 10
-while lstr(fi(N))<1000:
+while lstr(fi(N)) < 1000:
+    a = N
     N *= 2
     print(N)
 
-while lstr(fi(N))>999:
-    N -= 1
+coef = [a, N]
+while lstr(fi(N)) != 1000:
+    N = sum(coef)//2
+    if lstr(fi(N)) < 1000:
+        coef[0] = N
+    else:
+        coef[1] = N
     print(N)
+
+while lstr(fi(N)) > 999:
+    N -= 1
     if lstr(fi(N)) == 999:
         break
-print(N+1)
+    print(N)
